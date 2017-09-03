647f5b5e9060ecfa6bcef2903438a53be046c594
Dan Beil <dan@webdevstudios.com>
2017-09-03 10:21:25 -0500

    updating user import
    this commit has random debugging code in it

--- inc/class-wpcli-migration-user.php
+++ inc/class-wpcli-migration-user.php
@@ -7,9 +7,15 @@
 
 class WPCLI_Migration_User {
 
-	// public function __construct() {
+	/**
+	 * Our debugging property
+	 * @var Boolean
+	 */
+	public $debug;
 
-	// }
+	public function __construct( $debug ) {
+		$this->debug = $debug;
+	}
 
 	public function check_create_user( $import_post ) {
 
@@ -21,35 +27,47 @@ class WPCLI_Migration_User {
 		 */
 
 		if ( property_exists( $import_post->_links, 'author' ) ) {
+			WP_CLI::log( '1' );
 			// @codingStandardsIgnoreStart
 			$author = wp_remote_get( $import_post->_links->author[0]->href );
 			// @codingStandardsIgnoreEnd
 		} else {
+			WP_CLI::log( '2' );
 			$author = new WP_Error();
 		}
 
 		if ( ! is_wp_error( $author ) ) {
+			WP_CLI::log( '3' );
 			$author = json_decode( $author['body'] );
 			if ( property_exists( $author, 'name' ) ) {
+				WP_CLI::log( '4' );
 				$user = get_user_by( 'login', $author->name );
 			} else {
-				$author->name = 'null';
+				WP_CLI::log( '5' );
+				$author->name = null;
 			}
 		} else {
+			WP_CLI::log( '6' );
 			$user = false;
 		}
 
 		if ( false === $user ) {
+			WP_CLI::log( '7' );
 
 			if ( true === $this->debug ) {
 				WP_CLI::log( 'user ' . $author->name . ' does not exist we should create them' );
 			}
+
+echo "author\n<pre>";
+print_r($author);
+echo "</pre>\n\n";
 
 			if ( property_exists( $author , 'name' ) ) {
 				$new_user = wp_insert_user( array(
 					'user_login' => $author->name,
 					'user_name' => $author->name,
 					'user_pass' => wp_generate_password( 12, false ),
+					'role' => $author->role,
 				) );
 
 				/**
@@ -78,13 +96,22 @@ class WPCLI_Migration_User {
 				// @codingStandardsIgnoreEnd
 			}
 		} else {
+
+			WP_CLI::log( 'User already exists' );
+
 			$new_user = $user->data->ID;
+
+echo "new_user\n<pre>";
+print_r($new_user);
+echo "</pre>\n\n";
 
 			wp_update_user( array(
 				$new_user,
 				'display_name' => $user->name,
 			) );
 		}// End if().
+
+		return $new_user;
 
 		/**
 		 * End move this
